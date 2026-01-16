572fdb482b66c9fd278267bab2782fc420c190b0
0xluckycoder <lakshanperera.dev@gmail.com>
2026-01-16 23:35:58 +0530

    chore: update exports

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,14 +1,4 @@
 export { Scraper } from "./sdk";
-export { app, startServer } from "./../../api/index";
-export { Transformer } from "./transformer";
+export { app, startServer } from "../../api/index";
 export { ScrapeError, EngineError, ConfigError } from "./errors";
-
-export type {
-  ScrapeOptions,
-  ScrapedData,
-  OutputFormat,
-  AIMode,
-  PageMetadata,
-  RawContent,
-  RetryConfig,
-} from "./types";
+export type { ScrapeOptions, ScrapeResult, OutputFormat } from "./types";

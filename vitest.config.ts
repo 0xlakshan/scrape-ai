bdfa749e12c0d45c65cf73d312f3daa0776f2f7b
0xluckycoder <lakshanperera.dev@gmail.com>
2026-01-19 21:19:19 +0530

    chore: include type inference tests in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    typecheck: {
+      enabled: true,
+      include: ['**/*.test-d.ts'],
+    },
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html'],
       include: ['src/**/*.ts'],
-      exclude: ['src/index.ts', 'src/types.ts']
+      exclude: ['src/index.ts', 'src/types.ts', '**/*.test-d.ts']
     }
   }
 });
